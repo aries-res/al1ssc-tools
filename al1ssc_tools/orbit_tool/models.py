3cb181b3680a6bfff0abc297e0f94bd27823d2a7
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-08-31 00:13:14 +0530

    Added color & plot_by_default field to body model

--- al1ssc_tools/orbit_tool/models.py
+++ al1ssc_tools/orbit_tool/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Body(models.Model):
     name = models.CharField(max_length=200, unique=True)
     body_id = models.IntegerField()
+    color = models.CharField(max_length=200)
+    plot_by_default = models.BooleanField(default=True)
     # TODO: Add a sort field (whose default value is pk)
 
     def __str__(self):
