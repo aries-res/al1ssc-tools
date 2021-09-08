a91679a40fef37f7b2998c761ac98a80ba998ea0
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-09-08 22:53:30 +0530

    Changed url of 3D orbit tool

--- al1ssc_tools/orbit_tool/urls.py
+++ al1ssc_tools/orbit_tool/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_3Dorbit_data),
+    path("3D", views.get_3Dorbit_data),
     path("2D", views.get_2Dorbit_plot),
     path("bodies", views.get_bodies),
 ]
